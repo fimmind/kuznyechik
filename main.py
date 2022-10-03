03731e95d51e080eced6b16209208cb00a4874c2
fimmind <fimmind@mail.ru>
2022-10-03 19:25:45 +0300

    Better formatting for the example

--- main.py
+++ main.py
@@ -10,17 +10,17 @@ def main():
     KEY = 0x8899aabbccddeeff0011223344556677fedcba98765432100123456789abcdef
 
     msg = lorem.paragraph()
-    print(f"\nMessage:\n{msg}")
+    print(f"\n    Message:\n{msg}\n")
 
     IV, raw_enc_msg = encrypt_CTR(KEY, msg.encode(encoding='utf'))
-    print(f"\nRaw encrypted message:\n{raw_enc_msg}\n")
+    print(f"\n    Raw encrypted message:\n{raw_enc_msg}\n")
 
     raw_dec_msg = decrypt_CTR(KEY, IV, raw_enc_msg)
-    print(f"\nRaw decrypted message:\n{raw_dec_msg}\n")
+    print(f"\n    Raw decrypted message:\n{raw_dec_msg}\n")
 
     dec_msg = raw_dec_msg.decode('utf')
-    print(f"\nDecoded message:\n{dec_msg}\n"
-          f"\nEquals to the original?\n{msg == dec_msg}")
+    print(f"\n    Decoded message:\n{dec_msg}\n\n"
+          f"\nEquals to the original? {msg == dec_msg}")
 
 
 if __name__ == "__main__":
